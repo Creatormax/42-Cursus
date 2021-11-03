514ed62ae806dfb2fb515df47ff1d61d905856ef
hmorales <hctrfcomorales@gmail.com>
2021-11-03 15:25:20 +0100

    prueba

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmorales <hmorales@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:56:09 by hmorales          #+#    #+#             */
+/*   Updated: 2021/11/03 15:21:00 by hmorales         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define LIBFT;
+#ifdef LIBFT;
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *lol;
+    char i;
+
+    i = 0;
+    lol = (char *) malloc(3); 
+}
+
+#endif;
