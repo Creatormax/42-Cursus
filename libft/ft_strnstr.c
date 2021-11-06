66f6e893e2cfb90c4cedec74e193c19e3ebe2e39
hmorales <hctrfcomorales@gmail.com>
2021-11-06 23:49:41 +0100

    6/10/2021

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hmorales <hmorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 21:45:21 by hmorales          #+#    #+#             */
-/*   Updated: 2021/11/06 22:12:05 by hmorales         ###   ########.fr       */
+/*   Updated: 2021/11/06 23:49:08 by hmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	const	char	*b;
 	const	char	*l;
-	int				counter;
+	unsigned	int	counter;
+	unsigned	int	counter1;
 
 	b = big;
 	l = little;
+	counter1 = 0;
 	counter = 0;
 	if (*l == '\0')
 		return (b);
 	while (*b++ != '\0' && len-- > 0)
 	{
 		while (*b++ == *l++)
+			counter1++;
 		if (*l == '\0')
 			return (big + counter);
 		else
-			
+			while (counter1-- > 0)
 		counter++;
 	}
 	return (0);
