ed6313d06dc5a5b0f2c0826fded6ec689d4dce0c
hmorales <hctrfcomorales@gmail.com>
2021-11-19 00:05:28 +0100

    19/11/2021

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: hmorales <hmorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 13:04:15 by hmorales          #+#    #+#             */
-/*   Updated: 2021/11/18 23:47:10 by hmorales         ###   ########.fr       */
+/*   Updated: 2021/11/19 00:04:45 by hmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,5 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*nelem;
 
-	nelem = (t_list *)malloc(sizeof(t_list));
-	if (!nelem)
-		return (NULL);
-	nelem->content = content;
-	nelem->next = NULL;
-	return (nelem);
 }
