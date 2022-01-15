bdb693cc63f9aa461bdf398c52bef8e88c7d6b4b
Creatormax <hctrfcomorales@gmail.com>
2022-01-15 14:27:18 +0100

    GNL worked out 15/1/2022

--- GNL/get_next_line_utils.c
+++ GNL/get_next_line_utils.c
@@ -6,7 +6,81 @@
 /*   By: hmorales <hmorales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 14:39:04 by hmorales          #+#    #+#             */
-/*   Updated: 2021/12/17 14:39:05 by hmorales         ###   ########.fr       */
+/*   Updated: 2022/01/15 14:07:36 by hmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, int n)
+{
+	unsigned long int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(unsigned char *)(s + i) = '\0';
+		i++;
+	}	
+	return (s);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	str = (char *)s;
+	while (*str != (unsigned char)c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return (str);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+
+	d = dest;
+	s = src;
+	if (!dest && !src)
+		return (NULL);
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*str;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[j] = 0;
+	return (str);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
